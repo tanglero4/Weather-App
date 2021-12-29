0d15d42389b65e026e90901a4e508e08505bf658
Tiffany <tiffanyanglero94@hotmail.com>
2021-12-29 16:13:57 -0500

    Weather app files added
    The index.html, styles.css and javascript.js files have been added.

---
+++ index.html/javascript.js
@@ -0,0 +1,38 @@
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+h2.innerHTML = `${day} ${hour}:${minute}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city-input");
+  let footer = document.querySelector("footer");
+  footer.innerHTML = `${searchInput.value}`;
+  let apiKey = "f811ab7997433dcd9d82103a06077507";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", search);
+
+function convert(event) {
+  event.preventDefault();
+  let temperatureF = temperature[Math.round(temperature * 9) / 5 + 32];
+  let searchInput = document.querySelector("#farenheit");
+}
+let temperature = document.querySelector("#farenheit-converter");
+temperature.addEventListener("submit", convert);
+
+function showTemp(response) {
+  console.log(response.data.main.temp);
+  let temperature = Math.round(response.data.main.temp);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${temperature}°C`;
+}
